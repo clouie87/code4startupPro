bfd6626da226a1677a6a7a9a41c6834f25605538
clouie87 <carol.louie@gmail.com>
2015-06-02 16:13:48 -0300

    refactoring to add username to db

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true, length: {maximum: 25}
+  
 end
