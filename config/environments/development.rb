492cee52b4657efaa6e843ba4ce10624dbc0f31a
clouie87 <carol.louie@gmail.com>
2015-06-17 19:16:26 -0300

    trying to mkae heroku work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+  config.action_mailer.default_url_options = {host: 'https://powerful-sierra-9890.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.mandrillapp.com',
