d5b057b1048e63a9ec3f88bb21b34ebb6cdc089a
clouie87 <carol.louie@gmail.com>
2015-06-17 12:26:15 -0300

    changing ports

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+  config.action_mailer.default_url_options = {host: 'port:80'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.mandrillapp.com',
