8972b91aadf795fcd65804f5dbe99d96dbe66dc2
clouie87 <carol.louie@gmail.com>
2015-06-18 00:11:35 -0300

    digital ocean

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,6 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  config.assets.precompile += ['active_admin.js', 'active_admin.css']
-
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
